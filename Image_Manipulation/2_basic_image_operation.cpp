4ba901f94600b3558210f02b0736a8ccd574e8ec
YTHung <irock0608@gmail.com>
2020-12-28 23:08:45 +0800

    Add mask

--- Image_Manipulation/2_basic_image_operation.cpp
+++ Image_Manipulation/2_basic_image_operation.cpp
@@ -58,6 +58,72 @@ int Basic_Image_Operation() {
 
 
 	// Resize an image
+	// method 1: specify width and height
+	int resizeDownWidth = 300;
+	int resizeDownHeight = 200;
+	int resizeUpWidth = 600;
+	int resizeUpHeight = 900;
+	Mat resizeDown, resizeUp;
+	resize(image, resizeDown, Size(resizeDownWidth, resizeDownHeight), INTER_LINEAR);
+	resize(image, resizeUp, Size(resizeUpWidth, resizeUpHeight), INTER_LINEAR);
+	imshow("image", image);
+	imshow("image scaled up", resizeUp);
+	imshow("image scaled down", resizeDown);
+	waitKey(0);
+	destroyAllWindows();
+	// method 2: specify scaling fac
+	double scaleUpX = 1.5; 	// Scaling Down the image 1.5 times by specifying both scaling factors
+	double scaleUpY = 1.5;  // Scaling Down the image 1.5 times by specifying both scaling factors
+	double scaleDown = 0.6; // Scaling Down the image 0.6 times specifying a single scale factor.
+	Mat scaledUp, scaledDown;
+	resize(image, scaledDown, Size(), scaleDown, scaleDown, INTER_LINEAR);  // Size() : null
+	resize(image, scaledUp, Size(), scaleUpX, scaleUpY, INTER_LINEAR);
+	imshow("image scaled up", scaledUp);
+	imshow("image scaled down", scaledDown);
+	waitKey(0);
+	destroyAllWindows();
+
+
+
+	//------------------- Create an image mask -------------------
+	/*
+		Note:
+		In many computer image applications, we're not processing entire image but only parts of image. 
+		Sometimes it could be rectanglar, but most of the time it's not. So we use image mask to specify 
+		the region of interest that we're going to precess. 
+		The goal is to segment out the area of interest and apply your algorithm to a specific part of the image.
+		Usually, binary mask is used, where white pixcels are the region that we're interested in processing, 
+		and black represent the pixels we are not.
+	*/
+
+	// Create a mask using coordinates
+	Mat mask1 = Mat::zeros(image.size(), image.type());  // Create an empty image of same size as the original
+	imshow("mask1", mask1);
+	waitKey(0);
+	mask1(Range(50, 200), Range(170, 320)).setTo(255);   // Set roi
+	imshow("mask1", mask1);
+	waitKey(0);
+	destroyAllWindows();
+
+	// Create a mask using pixel intensity or colour
+	/*
+		Another way of creating masks is by using some logic. One example is using a simple color information. 
+		For example, Let us try to find out the pixels which are approximately red in color.
+
+		If we want to focus on red pixels, the simplest logic that does the trick is:
+
+		* The red channel should have high intensity ( keep the range of pixel values from 150 to 255 )
+		* The other 2 channels should have low intensity ( keep the range of pixel values in Blue and Green channels between 0 to 100)
+	*/
+	Mat mask2;
+	inRange(image, Scalar(0, 0, 150), Scalar(100, 100, 255), mask2); // lower boundary, upper boundary
+	imshow("mask2", mask2);
+	waitKey(0);
+	destroyAllWindows();
+
+	// As you can see the white portion in the output mask corressponds to the red pixels in the original image.
+
+
 
 	return 0;
 }
